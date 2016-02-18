87fb216fa9efa05119c139a60473bcc41298183b
Emily Heagy <emmykrishe@gmail.com>
2016-02-18 15:51:17 -0800

    Initial commit

---
+++ provision-post.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mkdir /var/www/wordpress/wp-content/themes/wp-salt-theme
+git clone https://github.com/Narthur/wp-salt-theme.git /var/www/wordpress/wp-content/themes/wp-salt-theme/
